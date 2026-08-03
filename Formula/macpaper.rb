3b6acc4bdbf65355b08bf93a02e97dd6ddb0882a
Anant Patel <66427020+anantdark@users.noreply.github.com>
2026-08-04 03:57:20 +0530

    Bump macpaper formula to v1.3.1.

--- Formula/macpaper.rb
+++ Formula/macpaper.rb
@@ -1,10 +1,10 @@
 class Macpaper < Formula
   desc "Register local videos and images in macOS Wallpaper and Screen Saver"
   homepage "https://github.com/anantdark/macpaper"
-  url "https://github.com/anantdark/macpaper/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "34ae3785da9f8d3ecc35cd52fd08b57e9b820bd43a7df927e4de2d0e39e0cfaa"
+  url "https://github.com/anantdark/macpaper/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "af56260827254c95dc770f5b7a3e4f4493e3cf4c47da8645398ee3763836d4d6"
   license "GPL-3.0-or-later"
-  version "1.3.0"
+  version "1.3.1"
 
   depends_on "ffmpeg"
   depends_on :macos
